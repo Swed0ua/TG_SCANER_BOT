262a88fbb3b69d9ba891f3259064c763556af072
Swed0ua <shmagala2001@gmail.com>
2025-06-24 23:43:05 +0300

    fix: load env in main file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 import asyncio
-
 from dotenv import load_dotenv
-from app.tg_bot.bot import start_tg_bot
 
 load_dotenv()
+
+from app.tg_bot.bot import start_tg_bot
+
 
 if __name__ == "__main__":
     asyncio.run(start_tg_bot())
